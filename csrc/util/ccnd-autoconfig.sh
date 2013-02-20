5cf5faa1f4ffbc17000d9f0b8909f0bed61c3e4d
Alexander Afanasyev <alexander.afanasyev@ucla.edu>
2013-02-19 16:21:46 -0800

    Refs #100852  Extending autoconfiguration script
    Now it is possible to run it in a daemon mode, which checks every 10 seconds
    existence of an autoconf route.  If not, it tries to configure it.

--- csrc/util/ccnd-autoconfig.sh
+++ csrc/util/ccnd-autoconfig.sh
@@ -1,10 +1,11 @@
 # Source file: util/ccnd-autoconfig.sh
-# 
+#
 # Script that tries to (automatically) discover of a local ccnd gateway
 #
 # Part of the CCNx distribution.
 #
 # Copyright (C) 2012 Palo Alto Research Center, Inc.
+#           (c) 2013 University of California, Los Angeles
 #
 # This work is free software; you can redistribute it and/or modify it under
 # the terms of the GNU General Public License version 2 as published by the
@@ -18,38 +19,73 @@
 D=`dirname "$0"`
 export PATH="$D:$PATH"
 
-ccndstatus | grep 224.0.23.170:59695 > /dev/null
-MCAST_EXISTED=$?
+function do-need-to-reconfig {
+    face=`ccndstatus | grep "ccnx:/autoconf-route face" | awk '{print $3}'`
+    if [ "x$face" == "x" ]; then
+        return 0
+    else
+        return 1
+    fi
+}
 
-# Removing any previously created (either by this script or ccndc srv command) default route
-for i in `ccndstatus | grep "ccnx:/autoconf-route face" | awk '{print $3}'`; do 
-   ccndc del / face $i
-   ccndc del /autoconf-route face $i
-done
+function run-autoconfig {
+    ccndstatus | grep 224.0.23.170:59695 > /dev/null
+    MCAST_EXISTED=$?
 
-# Set temporary multicast face
-ccndc -t 10 add "/local/ndn" udp  224.0.23.170 59695 
+    # Removing any previously created (either by this script or ccndc srv command) default route
+    for i in `ccndstatus | grep "ccnx:/autoconf-route face" | awk '{print $3}'`; do
+       ccndc del / face $i
+       ccndc del /autoconf-route face $i
+    done
 
-# Get info from local hub, if available
-info=`ccncat -s 2 /local/ndn/udp`
-if [ "x$info" = "x" ]; then
-   echo "Local hub is not availble, trying to use DNS to get local configuration"
-   # Try to use DNS search list to get default route information
-   ccndc srv
+    # Set temporary multicast face
+    ccndc -t 10 add "/local/ndn" udp  224.0.23.170 59695
 
-   if [ $MCAST_EXISTED -eq 1 ]; then
-      # destroying multicast face
-      ccndstatus | grep 224.0.23.170:59695 | awk '{print $2}' | xargs ccndc destroy face
-   fi
-   exit 1
-fi
+    # Get info from local hub, if available
+    info=`ccncat -s 2 /local/ndn/udp`
+    if [ "x$info" = "x" ]; then
+       echo "Local hub is not availble, trying to use DNS to get local configuration"
+       # Try to use DNS search list to get default route information
+       ccndc srv
 
-echo Setting default route to a local hub: "$info"
-echo "$info" | xargs ccndc add / udp
-echo "$info" | xargs ccndc add /autoconf-route udp
+       if [ $MCAST_EXISTED -eq 1 ]; then
+          # destroying multicast face
+          ccndstatus | grep 224.0.23.170:59695 | awk '{print $2}' | xargs ccndc destroy face
+       fi
+       return 1
+    fi
 
-if [ $MCAST_EXISTED -eq 1 ]; then
-   # destroying multicast face
-   ccndstatus | grep 224.0.23.170:59695 | awk '{print $2}' | xargs ccndc destroy face
+    echo Setting default route to a local hub: "$info"
+    echo "$info" | xargs ccndc add / udp
+    echo "$info" | xargs ccndc add /autoconf-route udp
+
+    if [ $MCAST_EXISTED -eq 1 ]; then
+       # destroying multicast face
+       ccndstatus | grep 224.0.23.170:59695 | awk '{print $2}' | xargs ccndc destroy face
+    fi
+}
+
+if [ "x$1" == "x-d" ]; then
+    run-autoconfig
+
+    PID=${2:-"/var/run/ccnd-autoconfig.pid"}
+    if test -f $PID &&  ps -p `cat $PID` >&-; then
+        # No need to run daemon, as it is already running
+        exit 0
+    fi
+
+    echo $$ > $PID
+
+    # Infinite loop with reconfig every 5 minutes
+    while true; do
+        if do-need-to-reconfig; then
+            echo "Trying to reconfigure automatic route..."
+            run-autoconfig
+        fi
+        sleep 10
+    done
+else
+    run-autoconfig
+    exit $?
 fi
 
